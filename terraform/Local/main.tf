dc87afade31962acf6868f06c8f8c46553dbab0c
Bimalkhimdung <bimalkhimdung@gmail.com>
2023-07-06 14:32:09 +0545

    commecnt added

--- terraform/Local/main.tf
+++ terraform/Local/main.tf
@@ -1,22 +1,24 @@
 terraform {
+# service provider section
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-
+#This aws version  section
   required_version = ">= 1.2.0"
 }
-
+ #this is  region section
 provider "aws" {
   region  = "us-west-2"
 }
-
+# Main resources defining sections 
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
+  #Name of instanc
   tags = {
     Name = "Terraform_Demo"
   }
