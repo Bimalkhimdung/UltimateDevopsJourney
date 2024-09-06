54267650401168b0677e2e948fc1ec1515b12ffe
Bimal Rai <bimalkhimdung@gmail.com>
2024-09-06 10:14:49 +0545

    pushed

---
+++ Python/loop/for_loop.py
@@ -0,0 +1,22 @@
+# _num = list(map(int, input("Enter any numbers: ").split()))
+#
+# square = []
+#
+# for values in _num:
+#     square_value = values**2
+#     square.append(square_value)
+# print(square)
+#
+# def _sq (val):
+#     return val ** 2
+# __num = list(map(int, input("enter numbers again :").split()))
+# _val=[]
+# _val = list(map(_sq, __num))
+# print(_val)
+#Tuples
+names = ("bimal","rai","khimdung")
+
+for i in range(len(names)):
+    print(names[i])
+    break
+
