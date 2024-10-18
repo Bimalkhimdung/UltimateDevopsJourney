d632e2bbf7dfd230ebc70b52577f55c2a4228cdd
bimalkhimdung <bimalkhimdung@gmail.com>
2024-10-18 17:30:22 +0545

    chnages

---
+++ Python/Dictinories/file1.py
@@ -0,0 +1,15 @@
+thisdict = {
+
+    "brand":"Tesla",
+    "model": "Y",
+    "Year": 2023,
+    "colors": ["red","1","black"]
+}
+
+#Print type
+# print(type(thisdict))
+print(thisdict)
+
+
+#constructordict = dict(brand = "Tata", model = "Eletric", Year = 2024)
+#print(constructordict)
